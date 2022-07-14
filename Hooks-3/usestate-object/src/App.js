8dae40a5c4710ce8a1f9add6768efde62cc77cf8
DevBhargav9669 <bhargavvarma96@gmail.com>
2022-07-14 11:14:19 +0530

    hooks practice

---
+++ Hooks-3/usestate-object/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HookCounter3 from './Components/HookCounter3';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounter3 />
+    </div>
+  );
+}
+
+export default App;
